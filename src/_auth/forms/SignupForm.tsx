1dd6ed6615ba06216687c48e3d814ee163397311
mohsensami <mohsensami88@gmail.com>
2023-10-28 21:54:00 +0330

    AuthLayout developed

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,5 +1,11 @@
+import { Button } from '@/components/ui/button';
+
 const SignupForm = () => {
-    return <div>SignupForm</div>;
+    return (
+        <div>
+            <Button>Click Me</Button>
+        </div>
+    );
 };
 
 export default SignupForm;
